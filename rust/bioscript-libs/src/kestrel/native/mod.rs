cb91ffe655cf622e2c70dbdd3679c0d040af9e0d
Madhava Jay <me@madhavajay.com>
2026-05-11 19:56:46 +1000

    Add native Kestrel active region detector

--- rust/bioscript-libs/src/kestrel/native/mod.rs
+++ rust/bioscript-libs/src/kestrel/native/mod.rs
@@ -1,9 +1,13 @@
 mod active_region;
+mod detector;
 mod kmer;
 mod variant;
 mod vcf;
 
 pub use active_region::{ActiveRegion, RegionStats};
+pub use detector::{
+    ActiveRegionDetection, ActiveRegionDetectorConfig, detect_active_regions, difference_threshold,
+};
 pub use kmer::{KmerCountMap, count_fastq_kmers, count_sequence_kmers};
 pub use variant::{NativeVariantCall, ReferenceRegion, VariantKind};
 pub use vcf::{KestrelVcfWriter, ReferenceSequence, VariantCall};
